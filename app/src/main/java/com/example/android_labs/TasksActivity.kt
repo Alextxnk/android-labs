5ad180a9924ce7e7f9a74323f87eb77c80c4faeb
Alextxnk <alexsolov766@gmail.com>
2023-02-07 13:21:16 +0300

    laba3-initial

--- app/src/main/java/com/example/android_labs/TasksActivity.kt
+++ app/src/main/java/com/example/android_labs/TasksActivity.kt
@@ -3,14 +3,16 @@ package com.example.android_labs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlin.system.exitProcess
 
-class MainActivity : AppCompatActivity() {
+class TasksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tasks)
     }
 
-    fun onClickTasks(view: View) {
-        setContentView(R.layout.activity_tasks)
+    fun onClickMain(view: View) {
+        moveTaskToBack(true);
+        exitProcess(-1)
     }
 }
